5307f535cff3054f8e2b47cca095f824bc4205a8
Jacobo <jacob.et.cetera@gmail.com>
2017-03-21 00:01:03 -0600

    Initial Commit

---
+++ Wall-E/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Wall-E
+//
+//  Created by Jacobo Tapia on 21/03/17.
+//  Copyright © 2017 Jacobo Tapia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
